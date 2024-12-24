0dfd50dce003855d8a552c9178f91b9f62b9957e
Pablo Lambert <pablomartin.lambert@gmail.com>
2024-12-24 00:27:00 -0300

    Subida desafio

---
+++ backend/script.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE softjobs;
+\c softjobs;
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL,
+    rol VARCHAR(25),
+    lenguage VARCHAR(20)
+);
+SELECT * FROM usuarios;
